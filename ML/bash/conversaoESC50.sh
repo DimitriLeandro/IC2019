f8da5cf1bd75b9b19e1ce625d7445c8ff0b03291
Dimitri Leandro <dimitri.leandro@gmail.com>
2020-06-29 19:05:16 -0300

    Profundidade de bits dos datasets estava errada
    O librosa não preserva a profundidade de bits dos áudios quando salva um novo WAV. A remoção de silêncio do SESA fez os áudios irem de PCM_U8 para FLOAT. Os datasets foram corrigidos e serão colocados no google drive.

--- ML/bash/conversaoESC50.sh
+++ ML/bash/conversaoESC50.sh
@@ -1,5 +1,18 @@
 # O ESC 50 SERA CONVERTIDO PARA 16KHZ 8BITS
 # O VOLUME FOI AJUSTADO PARA 75% PARA EVITAR CLIPAGEM
-for file in /home/dimi/Downloads/datasets/ESC_50_original/audio/*.wav; do 
-	sox -v 0.75 $file -c 1 -r 16000 -b 8 /home/dimi/Downloads/datasets/ESC_50_16kHz_8bits/audio/$(basename $file);
+
+for file in /home/dimi/Downloads/datasets/SESA_v2_16kHz_8bits/original_float/*.wav; do 
+	sox -v 0.9 $file -c 1 -r 16000 -b 8 /home/dimi/Downloads/datasets/SESA_v2_16kHz_8bits/original/$(basename $file);
+done
+
+for file in /home/dimi/Downloads/datasets/SESA_v2_16kHz_8bits/silencio_float/*.wav; do 
+	sox -v 0.9 $file -c 1 -r 16000 -b 8 /home/dimi/Downloads/datasets/SESA_v2_16kHz_8bits/silencio/$(basename $file);
+done
+
+for file in /home/dimi/Downloads/datasets/SESA_v2_16kHz_8bits/original_float/*.wav; do 
+	sox -v 0.9 $file -c 1 -r 16000 -b 16 /home/dimi/Downloads/datasets/SESA_v2_16kHz_16bits/original/$(basename $file);
+done
+
+for file in /home/dimi/Downloads/datasets/SESA_v2_16kHz_8bits/silencio_float/*.wav; do 
+	sox -v 0.9 $file -c 1 -r 16000 -b 16 /home/dimi/Downloads/datasets/SESA_v2_16kHz_16bits/silencio/$(basename $file);
 done
